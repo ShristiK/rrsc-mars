92ec96985c73a4b24dd2d106432d89025fbef2f0
N Vishnuteja Reddy <ashanthrokser22@gmail.com>
2018-06-07 11:48:20 +0530

    First Iter

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import modules.preproc as pp
 
 # Main function
 def main():
-    img = cv2.imread('images/sample/Sample_1_ESP_022855_1270.jpg')
-    # res1 = ca.watershed(img)
-    # res1 = ca.houghTransform(img)
-    res1 = areaFilter(img)
-    
-    # kernel = np.ones((3,3), np.uint8)
-    # res1 = cv2.morphologyEx(res1, cv2.MORPH_OPEN, kernel)
+    img1 = cv2.imread('images/sample/Sample_1_ESP_045812_1865.jpg')
+    img2 = cv2.imread('images/sample/Sample_1_ESP_022855_1270.jpg')
+    res1 = ca.houghTransform(img1)
+    res2 = ca.watershed(img2)
+    org2 = cv2.imread('images/sample/Sample_1_ESP_022855_1270.jpg')
+
+    cv2.imshow('Original1', img1)
     cv2.imshow('Result1', res1)
     cv2.waitKey(0)
+    cv2.imshow('Original2', org2)
+    cv2.imshow('Result2', res2)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
 
 if(__name__ == '__main__'):
     main()
--- modules/crateralgo.py
+++ modules/crateralgo.py
@@ -3,14 +3,13 @@ import numpy as np
 import pywt 
 
 # Default mode is Haar and level = 1
-def waveletTransform(img , mode = 'haar', level = 1):
+def waveletTransform(img , mode = 'haar', level = 3):
     ## Datatype conversions
     # Convert image to grayscale
-    # img  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Convert to float
     img  =  np.float32(img)   
     img /= 255
-    
     # Compute coefficients for transformation
     coeffs = pywt.wavedec2(img, mode, level=level)
 
